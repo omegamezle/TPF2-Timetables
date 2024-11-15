70c7d14dccb5bcb8d27bdd332199d96d97c5488b
Gregory365 <gregory_halls@outlook.com>
2024-11-15 18:02:58 +0000

    Revert "Stability fix (#75)"
    This reverts commit 5d8185cd77664525b52e7c9974670ce2f05a0715.

--- res/scripts/celmi/timetables/guard.lua
+++ res/scripts/celmi/timetables/guard.lua
@@ -1,11 +1,11 @@
 local guard = { }
 
 function guard.againstNil(parameter)
-	if (parameter == nil) then
-		local calling_func = debug.getinfo(2)
-		print("Timetables 1.3 ERROR - " .. calling_func.name .. "() - nil parameter")
-		print(debug.traceback())
-	end
+    if (parameter == nil) then
+        local calling_func = debug.getinfo(2)
+        print("Timetables 1.3 ERROR - " .. calling_func.name .. "() - nil parameter")
+        print(debug.traceback())
+    end
 end
 
 return guard
